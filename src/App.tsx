51be5665e882fba8679afa739b3ed1b0c9463515
douglasleao112 <132621816+douglasleao112@users.noreply.github.com>
2026-06-21 13:04:40 -0300

    123

--- src/App.tsx
+++ src/App.tsx
@@ -558,14 +558,11 @@ export default function App() {
       {/* 12. SEÇÃO CONSTRUIR JUNTOS O PRÓXIMO NÍVEL (FLEX ATENDIMENTO) */}
       <section className="py-24 bg-white text-slate-950 bg-grid-pattern-dark relative border-y border-slate-200 flex flex-col justify-center items-center overflow-hidden">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 text-center space-y-8 relative z-10">
-          <img 
-  src="https://i.ibb.co/S8XddB6/flex-para-fundo-escuro.png" 
+     <img 
+  src="https://i.ibb.co/mV3TmZJh/flex-logo.png" 
   alt="Flex Fitness" 
   referrerPolicy="no-referrer"
   className="h-14 sm:h-16 object-contain mx-auto select-none"
-  style={{
-    filter: "brightness(0) saturate(100%) invert(9%) sepia(85%) saturate(3300%) hue-rotate(240deg) brightness(75%) contrast(110%)"
-  }}
 />
           
           <h2 className="font-display font-black text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-[#170d66] leading-tight uppercase tracking-tight max-w-3xl mx-auto">
